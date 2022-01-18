84ac7526e18a9048e3510b37ff972cecbb5d255f
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2022-01-18 11:19:38 -0300

    feat.: Added code

---
+++ pico-torrent.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+'''
+pico-torrent 1.0
+
+A fast and small torrent client, initially made for Otto-PKG.
+Developed by: Jazz_Man (alias).
+Applied from the following example: https://www.libtorrent.org/python_binding.html
+
+License:
+Copyright © MMXX Projeto Pindorama.
+pico-torrent is under the Caldera license.
+
+Dependencies:
+ > python 3.4=<
+ > libtorrent-rasterbar
+
+Syntax:
+python3.? pico-torrent -f <file> -d <directory> (-s| )
+
+To-do list:
+ > Manually set up the download bandwidth priority;
+ > Use magnet link instead of file;
+ > Create .torrent files;
+ > Cryptography support (perhaps).
+'''
+
+import libtorrent as lt
+import argparse, os
+
+
+argument = argparse.ArgumentParser()
+argument.add_argument("--file", "-f", required=True,
+                       help="Torrent file to download")
+argument.add_argument("--seeding", "-S", action="store_true",
+                       help="Keep seeding after the download is complete")
+argument.add_argument("--infos", "-i",
+                       help="Show torrent info")
+argument.add_argument("--output", "-O", action="store_true",
+                       help="File output; filename")
+#argument.add_argument("--download-priority", "-P", default=lt.pri,
+#                       help="Set download priority")
+#argument.add_argument("--link", "-L",
+#                       help="Create a magnet link through a file")
+argument.add_argument("--dir", "-d",
+					   help="Specify the download save directory")
+arguments = argument.parse_args()
+
+session = lt.session({'listen_interfaces': '0.0.0.0:10881'})
+
+
+def start_download(filename):
+    try:
+        info = lt.torrent_info(filename)
+        if arguments.dir:
+            if os.path.isdir(arguments.dir):
+                local = session.add_torrent({'ti': info, 'save_path': arguments.dir})
+            else:
+                print(">>> Invalid Directory")
+        else:
+            local = session.add_torrent({'ti': info, 'save_path': '/tmp/otto_pkg/'})
+        torrent = local.status()
+        if arguments.file and arguments.seeding:
+            while True:
+                torrent = local.status()
+                print("\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s" % (
+                 torrent.progress * 100, torrent.download_rate / 1000, torrent.upload_rate / 1000,
+                 torrent.num_peers, torrent.state), end=" ")
+            print("\n>>> Download complete.\n>>>Seeding")
+        elif arguments.file:
+            while not torrent.is_seeding:
+                torrent = local.status()
+                print("\r%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s" % (
+        		 torrent.progress * 100, torrent.download_rate / 1000, torrent.upload_rate / 1000,
+        		 torrent.num_peers, torrent.state), end=" ")
+        print("\n>>> Download complete!")
+    except KeyboardInterrupt:
+        print("\n>>> Exiting...\n")
+    except Exception as error:
+        print(">>> There was an error: {}".format(error))
+
+start_download(arguments.file)
