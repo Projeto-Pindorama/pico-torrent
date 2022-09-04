8a50dcee9cee32db30cf1a8ff7bf7798065acdec
vitorhcl <vitorhcl00@gmail.com>
2022-09-04 00:22:34 -0300

    refactor: move argument-related code to __main__ block

--- pico-torrent.py
+++ pico-torrent.py
@@ -27,29 +27,13 @@ To-do list:
 '''
 
 import libtorrent as lt
-import argparse
 import os
 
 
-argument = argparse.ArgumentParser()
-exclusive = argument.add_mutually_exclusive_group(required=True)
-exclusive.add_argument("--file", "-f",help="Torrent file or magnet link to download")
-argument.add_argument("--seeding", "-S", action="store_true",help="Keep seeding after the download is complete")
-argument.add_argument("--infos", "-i",help="Show torrent info")
-argument.add_argument("--output", "-O", action="store_true",help="File output; filename")
-# argument.add_argument("--download-priority", "-P", default=lt.pri,
-#                       help="Set download priority")
-exclusive.add_argument("--link", "-L",help="Create a magnet link through a file")
-argument.add_argument("--dir", "-d",help="Specify the download save directory")
-arguments = argument.parse_args()
-
-session = lt.session({'listen_interfaces': '0.0.0.0:10881'})
-
-
-def get_save_dir():
-    if arguments.dir:
-        if os.path.isdir(arguments.dir):
-            return arguments.dir
+def get_save_dir(dir):
+    if dir:
+        if os.path.isdir(dir):
+            return dir
         print(">>> Invalid Directory")
     else:
         return os.getcwd()
@@ -62,21 +46,21 @@ def show_status(local):
         torrent.num_peers, torrent.state), end=" ")
 
 
-def start_download(filename):
+def start_download(filename, keep_seeding, save_dir):
     try:
         if os.path.isfile(filename):
-            params = {'save_path': get_save_dir(
+            params = {'save_path': get_save_dir(save_dir
             ), 'ti': lt.torrent_info(filename)}
         else:
             params = lt.parse_magnet_uri(filename)
             params.save_path = get_save_dir()
         local = session.add_torrent(params)
         torrent = local.status()
-        if arguments.file and arguments.seeding:
+        if filename and keep_seeding:
             while True:
                 show_status(local)
             print("\n>>> Download complete.\n>>>Seeding")
-        elif arguments.file:
+        elif filename:
             while not torrent.is_seeding:
                 show_status(local)
         print("\n>>> Download complete!")
@@ -90,7 +74,21 @@ def create_magnet(filename):
     return lt.make_magnet_uri(lt.torrent_info(filename))
 
 
-if arguments.link:
-    print(create_magnet(arguments.link))
-elif arguments.file:
-    start_download(arguments.file)
+if __name__ == "__main__":
+    import argparse
+    argument = argparse.ArgumentParser()
+    exclusive = argument.add_mutually_exclusive_group(required=True)
+    exclusive.add_argument("--file", "-f",help="Torrent file or magnet link to download")
+    argument.add_argument("--seeding", "-S", action="store_true",help="Keep seeding after the download is complete")
+    argument.add_argument("--infos", "-i",help="Show torrent info")
+    argument.add_argument("--output", "-O", action="store_true",help="File output; filename")
+    # argument.add_argument("--download-priority", "-P", default=lt.pri,
+    #                       help="Set download priority")
+    exclusive.add_argument("--link", "-L",help="Create a magnet link through a file")
+    argument.add_argument("--dir", "-d",help="Specify the download save directory")
+    arguments = argument.parse_args()
+
+    if arguments.link:
+        print(create_magnet(arguments.link))
+    elif arguments.file:
+        start_download(arguments.file, arguments.seeding, arguments.dir)
